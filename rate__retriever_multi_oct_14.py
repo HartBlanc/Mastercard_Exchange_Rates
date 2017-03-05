f697dc3ebf2dd8cf5a64f98c764a1b967bf4492d
Callum Hart <callumhartemail@gmail.com>
2017-03-05 17:35:09 +0900

    Update rate__retriever_multi_oct_14.py
    Removed unnecessary packages
    Added annotations
    Replaced from_c and to_c in 'entry' to from_id and to_id
    Moved now and today above date_1 in the first_date reset.

--- rate__retriever_multi_oct_14.py
+++ rate__retriever_multi_oct_14.py
@@ -1,5 +1,6 @@
 # 151 currencies, 22650 currency pairs, 364 days (period 1) 134 days (period 2) => 3,035,100(20,100/from_c) - 8,221,950 entries(8361.157)
 
+#####IMPORT PACKAGES
 
 print('importing packages')
 import time
@@ -7,15 +8,17 @@ import sqlite3
 import json
 import requests
 import datetime
-import math
 import pytz
 from datetime import date
 from multiprocessing.pool import Pool
-import sys
+
+###### Create/connect to database (sqlite file)
 
 print('connecting to db')
 conn = sqlite3.connect('mastercard_db.sqlite')
 cur = conn.cursor()
+
+###### Defining Functions, Convert dates into different types, date/day/string, chunkIt splits currency codes into approximately equal chunks
 
 print('defining functions')
 def day_calculator(date):
@@ -35,17 +38,17 @@ def chunkIt(seq, num):
 
   return out
     
-
+###### Defining constants, first_date is first_date in database, now is datetime in EST, today is last date rates available for, date_1 is first date rates available for
+    
 print('defining constants')
 start_from_id = int(input('from_id initial value: '))
-start_to_ids= [28]*3+[27]*12
+start_to_ids= [int(x) for x in input('List of to_ids, seperated by spaces').split()]
 n = len(start_to_ids)
 
 base_url = 'https://www.mastercard.us/settlement/currencyrate/fxDate={date_};transCurr={from_};crdhldBillCurr={to_};bankFee=0.00;transAmt=1/conversion-rate'
 
 first_date=date(2016,2,29)
 now = datetime.datetime.now(pytz.timezone('US/Eastern'))
-print('now: ', now)
 if now.hour < 14:
     today=now.date() - datetime.timedelta(days=1)
 else:
@@ -68,6 +71,7 @@ code_tuples=cur.fetchall()
 codes = [ x[0] for x in code_tuples ]
 number_of_codes = len(codes)
 
+######### Extracts all exchnage rates from 14th Oct onward
 
 def extract_rates(from_id,to_id):
     if to_id>151:
@@ -78,7 +82,9 @@ def extract_rates(from_id,to_id):
         entry='done'
         entries.append(entry)
         return entries   
-      
+    
+    ######### Creates URL
+    
     else:
         from_c = codes[from_id-1]
         to_c = codes[to_id-1]
@@ -99,7 +105,8 @@ def extract_rates(from_id,to_id):
                     time.sleep(5) 
                     continue
                 break
-    
+            ######### Error Handling
+            
             if 'errorCode' in JSON['data']:
                 if JSON['data']['errorCode'] in ('104','114'):
                     print('data not available for this date')
@@ -116,11 +123,14 @@ def extract_rates(from_id,to_id):
                     print('error code: ',JSON['data']['errorCode'])
                     print('conversion rate too small')
                     break
+            
+            ######### Adds conversion rate with date_id and currency ids
+            
             else:
                 rate = JSON['data']['conversionRate']
                 day = day_calculator(date)     
                 date_id=(date_1-first_date).days+day
-                entry=(rate,from_c,to_c,date_id)
+                entry=(rate,from_id,to_id,date_id)
                 entries.append(entry)
                 day+=1
                 date=date_calculator(day)
@@ -150,7 +160,9 @@ for code in codes[(start_from_id-1):]:
                 print(from_ids[i],to_ids[i])
 
             start_time = datetime.datetime.now()     
+            ### Multithread with n threads
             p = Pool(processes=n)
+            ### Returns list of n lists of entries for the year for n currency codes
             entries_list =p.starmap(extract_rates, [(from_ids[x],to_ids[x]) for x in range(0,n) ])
             p.close()
             for entries in entries_list:
@@ -161,22 +173,25 @@ for code in codes[(start_from_id-1):]:
                         cur.execute('''INSERT OR REPLACE INTO Rates
                         (rate, from_id, to_id, date_id)
                         VALUES ( ?, ?, ?, ?)''', 
-                        (entry[0], codes.index(entry[1])+1, codes.index(entry[2])+1, entry[3]) )
+                        (entry[0], entry[1], entry[2], entry[3]) )
                 conn.commit()
                 end_time = datetime.datetime.now()
                 print('Duration: {}'.format(end_time - start_time))
             to_ids[:] = [x+1 for x in to_ids]
-
-            date_1=today - datetime.timedelta(days=364)
-            if date_1.weekday()==6:
-                date_1=date_1+datetime.timedelta(days=1)
-            if date_1.weekday()==5:
-                date_1=date_1+datetime.timedelta(days=2)
+            
+            ### Updates current date to ensure that if time has passed still collecting data for all available dates
+            
             now = datetime.datetime.now(pytz.timezone('US/Eastern'))
             if now.hour < 14:
                 today=now.date() - datetime.timedelta(days=1)
             else:
                 today=now.date()
+            date_1=today - datetime.timedelta(days=364)
+            if date_1.weekday()==6:
+                date_1=date_1+datetime.timedelta(days=1)
+            if date_1.weekday()==5:
+                date_1=date_1+datetime.timedelta(days=2)
+
 
         from_ids[:] = ['done' if from_ids[x] in ('done',last_from_ids[x]) else from_ids[x]+1  for x in range(0,n)]
         print (from_ids)
